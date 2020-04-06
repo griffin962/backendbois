a949416921886e69e43630a65b24e29a9df417fb
Jonathan Miller <jam4x2@mst.edu>
2020-04-06 18:38:06 -0500

    Add json function for sql objects

--- klap4/db_entities/__init__.py
+++ klap4/db_entities/__init__.py
@@ -8,6 +8,22 @@ from sqlalchemy.sql.expression import and_
 # Base SQLAlchemy ORM class.
 SQLBase = declarative_base()
 
+
+def get_json(sql_object: SQLBase) -> dict:
+    dict_data = vars(sql_object).copy()
+    dict_data.pop("_sa_instance_state")
+    dict_data["id"] = sql_object.id
+    return dict_data
+
+
+# class JSONable:
+#     def __json__(self):
+#         raise NotImplementedError(f"Class {type(self).__name__} does not implement the .json() interface.")
+#
+#     def json(self):
+#         return self.__json__()
+
+
 KLAP4_TAG = namedtuple("KLAP4_TAG", ["genre_abbr",
                                      "artist_num",
                                      "album_letter",
