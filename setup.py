bdad2f8fcc3342e91681c676071c2d1b392b592f
Jonathan Miller <jam4x2@mst.edu>
2020-04-05 22:00:13 -0500

    Fix klap4 module version string and also add it to Pipenv.lock

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-import re
 import setuptools
 
-module_dir = Path(__file__).resolve().parent
+import klap4
+
+current_dir = Path(__file__).absolute().parent
 
 # KLAP4 module short description.
 short_description = "KMNR Music Library Interface"
 
-# Try and read the klap4 module's version.
-with (module_dir/"__init__.py").open('r') as module_file:
-    try:
-        module_version = re.search("^__version__ = \"((\d.?)+)\"$", module_file.read()).group(1)
-    except AttributeError:
-        module_version = "0.0.0"
 try:  # Try and read the README.md file for the long description.
-    with (module_dir.parent/"README.md").open('r') as readme_file:
+    with (current_dir.parent/"README.md").open('r') as readme_file:
         long_description = readme_file.read()
 except FileNotFoundError:
     long_description = short_description
 
+
 setuptools.setup(
     name="KLAP 4",
-    version=module_version,
+    version=klap4.__version__,
     description=short_description,
     long_description=long_description,
     long_description_content_type="text/markdown",
