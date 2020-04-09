a4150776bb7cce609e4e6178cb10aa442053b864
Jonathan Miller <jam4x2@mst.edu>
2020-04-09 15:14:34 -0500

    Move dummy data seed script to examples folder

--- Examples/seed_db.py
+++ Examples/seed_db.py
@@ -11,7 +11,7 @@ from klap4.db_entities import *
 def main():
     script_dir = Path(__file__).absolute().parent
     klap4.db.connect(script_dir/".."/"test.db", reset=True)
-    yaml_files = [path.name for path in script_dir.glob('*.yaml')]
+    yaml_files = [path.name for path in (script_dir/".."/"Dummy DB Data").glob('*.yaml')]
     print(f"Loaded files: {yaml_files}")
     fixtures = load_fixture_files(script_dir, yaml_files)
     load_fixtures(klap4.db.Session, fixtures)
